b81a3445291087593e6443a00ac61f92a6aaf244
286178 <Charan.A@ust.com>
2024-10-25 10:32:48 +0530

    intial

---
+++ src/main/java/com/ust/app/api/MyApi.java
@@ -0,0 +1,12 @@
+package com.ust.app.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyApi {
+
+    public  String sayHello(){
+        return "Hello world";
+    }
+}
