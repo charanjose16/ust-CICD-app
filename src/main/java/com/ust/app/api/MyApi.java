3841ca591d565897e456f62e36da2910a876e9be
286178 <Charan.A@ust.com>
2024-10-25 12:33:17 +0530

    update greeting message

--- src/main/java/com/ust/app/api/MyApi.java
+++ src/main/java/com/ust/app/api/MyApi.java
@@ -1,12 +1,13 @@
 package com.ust.app.api;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyApi {
 
+    @GetMapping
     public  String sayHello(){
-        return "Hello world";
+        return "Hello!";
     }
 }
